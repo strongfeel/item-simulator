b9dfa8abdb573ada5da217507a7f45099de9202f
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-09-09 19:53:06 +0900

    캐릭터 삭제 API 구현

--- src/routes/characters.routers.js
+++ src/routes/characters.routers.js
@@ -9,6 +9,14 @@ router.post("/characters", authMiddleware, async (req, res, next) => {
   const { userId } = req.user;
   const { characterName, health, power, money } = req.body;
 
+  const isExistCharterName = await prisma.characters.findFirst({
+    where: { characterName },
+  });
+
+  if (isExistCharterName) {
+    return res.status(404).json({ message: "이미 존재하는 닉네임 입니다." });
+  }
+
   const character = await prisma.characters.create({
     data: {
       userId: +userId,
@@ -21,5 +29,30 @@ router.post("/characters", authMiddleware, async (req, res, next) => {
 
   return res.status(201).json({ data: character });
 });
+
+// 캐릭터 삭제 API 구현
+router.delete(
+  "/characters/:characterId",
+  authMiddleware,
+  async (req, res, next) => {
+    const { characterId } = req.params;
+    const { userId } = req.user;
+
+    const character = await prisma.characters.findFirst({
+      where: { characterId: +characterId },
+    });
+    if (!character)
+      return res.status(404).json({ message: "캐릭터가 존재하지 않습니다." });
+
+    const deleteCharacter = await prisma.characters.delete({
+      where: {
+        characterId: +characterId,
+        userId: +userId,
+      },
+    });
+
+    return res.status(200).json({ data: deleteCharacter });
+  }
+);
 
 export default router;
